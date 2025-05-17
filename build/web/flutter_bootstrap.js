14a1c5e136a82227f52ca657256ec3994a9610e3
bervifix <simone.bervi@gmail.com>
2025-05-17 12:51:27 +0200

    Add photo assets and update image archive to use local images

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3671633870"
+    serviceWorkerVersion: "2488022112"
   }
 });
--- lib/screens/image_archive.dart
+++ lib/screens/image_archive.dart
@@ -1,126 +1,139 @@
 import 'package:flutter/material.dart';
-  import 'package:cached_network_image/cached_network_image.dart';
-  import 'package:photo_view/photo_view.dart';
-  import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:photo_view/photo_view.dart';
 
-  class ImageArchive extends StatelessWidget {
-    const ImageArchive({super.key});
+class ImageArchive extends StatelessWidget {
+  const ImageArchive({super.key});
 
-    static const _gql = r'''
-      query ImagesMedia {
-        assets(
-          kind: "image"
-          orderBy: "filename ASC"
-        ) {
-          url
-          filename
-        }
-      }
-    ''';
+  // Lista statica dei percorsi delle immagini in assets/photos
+  static const List<String> _assetImages = [
+    'assets/photos/img-1.jpg',
+    'assets/photos/img-2.jpg',
+    'assets/photos/img-3.jpg',
+    'assets/photos/img-4.jpg',
+    'assets/photos/img-5.jpg',
+    'assets/photos/img-6.jpg',
+    'assets/photos/img-7.jpg',
+    'assets/photos/img-8.jpg',
+    'assets/photos/img-9.jpg',
+    'assets/photos/img-10.jpg',
+    'assets/photos/img-11.jpg',
+    'assets/photos/img-12.jpg',
+    'assets/photos/img-13.jpg',
+    'assets/photos/img-14.jpg',
+    'assets/photos/img-15.jpg',
+  ];
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Archivio Immagini')),
-        body: Query(
-          options: QueryOptions(document: gql(_gql)),
-          builder: (result, {fetchMore, refetch}) {
-            if (result.isLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (result.hasException) {
-              return Center(child: Text('Errore: ${result.exception}'));
-            }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Archivio Immagini')),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+        ),
+        itemCount: _assetImages.length,
+        itemBuilder: (context, index) {
+          final assetPath = _assetImages[index];
 
-            final assets = result.data!['assets'] as List<dynamic>;
-
-            return GridView.builder(
-              padding: const EdgeInsets.all(8),
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                mainAxisSpacing: 8,
-                crossAxisSpacing: 8,
-              ),
-              itemCount: assets.length,
-              itemBuilder: (context, index) {
-                final asset = assets[index];
-                final url = asset['url'] as String;
-
-                return GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => _ImageViewer(
-                      assets: assets,
-                      initialIndex: index
-                    )),
-                  ),
-                  child: Hero(
-                    tag: 'image_$index',
-                    child: CachedNetworkImage(
-                      imageUrl: url,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) => const Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                      errorWidget: (context, url, error) => const Icon(Icons.error),
+          return GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => _ImageViewer(
+                  assetPaths: _assetImages,
+                  initialIndex: index
+              )),
+            ),
+            child: Hero(
+              tag: 'image_$index',
+              child: Image.asset(
+                assetPath,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  // Stampa l'errore specifico per debugging
+                  print('Errore caricamento: $assetPath - $error');
+                  return Container(
+                    color: Colors.grey[800],
+                    child: const Center(
+                        child: Icon(Icons.image_not_supported, color: Colors.white70)
                     ),
-                  ),
-                );
-              },
-            );
-          },
-        ),
-      );
-    }
-  }
-
-  class _ImageViewer extends StatefulWidget {
-    const _ImageViewer({required this.assets, required this.initialIndex});
-    final List<dynamic> assets;
-    final int initialIndex;
-
-    @override
-    State<_ImageViewer> createState() => __ImageViewerState();
-  }
-
-  class __ImageViewerState extends State<_ImageViewer> {
-    late PageController _controller;
-
-    @override
-    void initState() {
-      super.initState();
-      _controller = PageController(initialPage: widget.initialIndex);
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.black,
-        body: Stack(
-          children: [
-            PageView.builder(
-              controller: _controller,
-              itemCount: widget.assets.length,
-              itemBuilder: (context, index) {
-                final asset = widget.assets[index];
-                final url = asset['url'] as String;
-
-                return Hero(
-                  tag: 'image_$index',
-                  child: PhotoView(imageProvider: CachedNetworkImageProvider(url)),
-                );
-              },
-            ),
-            Positioned(
-              top: MediaQuery.of(context).padding.top + 8,
-              left: 8,
-              child: IconButton(
-                icon: const Icon(Icons.close, color: Colors.white),
-                onPressed: () => Navigator.pop(context),
+                  );
+                },
               ),
             ),
-          ],
-        ),
-      );
-    }
+          );
+        },
+      ),
+    );
   }
+}
+
+class _ImageViewer extends StatefulWidget {
+  const _ImageViewer({required this.assetPaths, required this.initialIndex});
+  final List<String> assetPaths;
+  final int initialIndex;
+
+  @override
+  State<_ImageViewer> createState() => __ImageViewerState();
+}
+
+class __ImageViewerState extends State<_ImageViewer> {
+  late PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(initialPage: widget.initialIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _controller,
+            itemCount: widget.assetPaths.length,
+            itemBuilder: (context, index) {
+              final assetPath = widget.assetPaths[index];
+
+              return Hero(
+                tag: 'image_$index',
+                child: PhotoView.customChild(
+                  backgroundDecoration: const BoxDecoration(color: Colors.black),
+                  child: Image.asset(
+                    assetPath,
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.broken_image, size: 64, color: Colors.white60),
+                            SizedBox(height: 16),
+                            Text('Immagine non disponibile',
+                                style: TextStyle(color: Colors.white70))
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 8,
+            left: 8,
+            child: IconButton(
+              icon: const Icon(Icons.close, color: Colors.white),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
